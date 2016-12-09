cb49539958d146908f52051eef84db50e9f6f564
thefriendlydev <matt@thefriendlydev.com>
2016-12-08 22:22:38 -0600

    gulp

--- wp-content/themes/ux4/assets/js/shiftNavOverrides.js
+++
@@ -1,6 +0,0 @@
-(function($) {
-  $( document ).ready(function() {
-    $('.shiftnav-toggle-main-block').empty()
-    $('.shiftnav-toggle-main-block').append("<a class='mobileLogo' href='/''><img src='/wp-content/themes/edi/dist/img/nav-logo.png'></a>")
-  });
-})(jQuery);
